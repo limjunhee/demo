d3ddca234c0ccbb1d361eccddac07e1d432798bb
limjunhee <ski725400@gmail.com>
2024-10-02 14:27:22 +0900

    SPRING_20211017

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller //컨트롤러 어노테이션 명시 파일을 다른 곳으로 옮겨도 자동으로 인식해 줌.
+
+public class DemoController {
+    @GetMapping("/hello") // 전송 방식 GET
+    public String hello(Model model) {
+        model.addAttribute("data", " 방갑습니다."); // model 설정
+        return "hello"; // hello.html 연결
+    }
+
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
+    }
+
+    @GetMapping("/test1")
+    public String thymeleaf_test1(Model model) {
+        model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+        model.addAttribute("data2", "태그의 속성 값");
+        model.addAttribute("link", 01);
+        model.addAttribute("name", "홍길동");
+        model.addAttribute("para1", "001");
+        model.addAttribute("para2", 002);
+        return "thymeleaf_test1";
+    }
+}
