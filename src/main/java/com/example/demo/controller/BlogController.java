a11fe63ce45fccd270686525e69b1a9b150d31a1
limjunhee <ski725400@gmail.com>
2024-10-16 14:09:04 +0900

    SPRING_20211017

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,19 +1,47 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
 @Controller //컨트롤러 어노테이션 명시 파일을 다른 곳으로 옮겨도 자동으로 인식해 줌.
 
 public class BlogController {
+
+    @Autowired // 객체 주입 자동화, 생성자 1개면 생략 가능
+    BlogService blogService; // 리포지토리 선언
     @GetMapping("/article_list") // 게시판 링크 지정
         public String article_list(Model model) {
-        List<Article> list = blogService.findAll(); // 게시판 리스트
-        model.addAttribute("articles", list); // 모델에 추가
+            List<Article> list = blogService.findAll(); // 게시판 리스트
+            model.addAttribute("articles", list); // 모델에 추가
         return "article_list"; // .HTML 연결
     }
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+        if (list.isPresent()) {
+            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+            } else {
+                // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+                return "error"; // 오류 처리 페이지로 연결
+                }
+                return "article_edit"; // .HTML 연결
+        }
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+        }
 }
