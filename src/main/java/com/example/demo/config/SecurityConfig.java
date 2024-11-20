20e71b4b9f074146b9c5fcf14d960239d24f8095
limjunhee <ski725400@gmail.com>
2024-11-20 12:34:37 +0900

    20211017_SPRING

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean // 명시적 의존성 주입 : Autowired와 다름
+    // 5.7버전 이저 WebSecurityConfigurerAdapter 사용
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // 설정을 비워둠
+    return http.build(); // 필터 체인을 통해 보안설정(HttpSecurity)을 반환
+    }
+    @Bean // 암호화 설정
+    public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(); // 비밀번호 암호화 저장
+    }
+
+}
